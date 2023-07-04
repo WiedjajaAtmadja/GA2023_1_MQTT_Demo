0fa4ac8e3b6ff2bd9f3cb707dccc3e7d90cdb58d
Wiedjaja Atmadja <swiedjaja@binus.ac.id>
2023-07-05 03:12:27 +0700

    final code

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,95 @@
 #include <Arduino.h>
 #include <DHTesp.h>
 #include <BH1750.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 #include "devices.h"
+
+#define WIFI_SSID "Steff-IoT" // ganti dengan SSID masing-masing
+#define WIFI_PASSWORD "steffiot123" // ganti dengan password masing-masing
+
+#define MQTT_BROKER  "broker.emqx.io"
+
+#define MQTT_TOPIC_PUBLISH_DEVICE_STATUS  "Binus_Syahdan/L1A/data/deviceStatus"
+#define MQTT_TOPIC_PUBLISH_TEMP           "Binus_Syahdan/L1A/data/Temperature"
+#define MQTT_TOPIC_PUBLISH_HUM            "Binus_Syahdan/L1A/data/Humidity"
+#define MQTT_TOPIC_PUBLISH_LIGHT          "Binus_Syahdan/L1A/data/Light"
+#define MQTT_TOPIC_PUBLISH_HELP_BUTTON    "Binus_Syahdan/L1A/data/HelpButton"
+#define MQTT_TOPIC_SUBSCRIBE              "Binus_Syahdan/L1A/cmd/#" 
+
+#define MQTT_TOPIC_CMD_LED_RED            "Binus_Syahdan/L1A/cmd/LedRed" 
+#define MQTT_TOPIC_CMD_LED_GREEN          "Binus_Syahdan/L1A/cmd/LedGreen" 
 
 DHTesp dht;
 BH1750 lightMeter;
+WiFiClient espClient;
+PubSubClient mqtt(espClient);
+
+void WifiConnect()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }  
+  Serial.print("System connected with IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.printf("RSSI: %d\n", WiFi.RSSI());
+}
+
+void mqttCommandRecv(char* topic, byte* payload, unsigned int len) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  Serial.write(payload, len);
+  Serial.println();
+  int nLed = 0;
+  if (strcmp(topic, MQTT_TOPIC_CMD_LED_RED)==0)
+    nLed = LED_RED;
+
+  if (strcmp(topic, MQTT_TOPIC_CMD_LED_GREEN)==0)
+    nLed = LED_GREEN;
+
+  int nLedStatus = payload[0] - '0';
+  if (nLed>0)
+    digitalWrite(nLed, nLedStatus);  
+}
+
+void mqttPublish(const char* szTopic, const char* szMessage)
+{
+  Serial.printf("Publishing message to topic: %s -> %s\n", szTopic, szMessage);
+  mqtt.publish(szTopic, szMessage);
+}
+
+boolean mqttConnect() {
+  char g_szDeviceId[30];
+  sprintf(g_szDeviceId, "esp32_%08X",(uint32_t)ESP.getEfuseMac());
+  mqtt.setServer(MQTT_BROKER, 1883);
+  mqtt.setCallback(mqttCommandRecv);
+  Serial.printf("Connecting to %s clientId: %s\n", MQTT_BROKER, g_szDeviceId);
+
+  boolean fMqttConnected = false;
+  for (int i=0; i<3 && !fMqttConnected; i++) {
+    Serial.print("Connecting to mqtt broker...");
+    fMqttConnected = mqtt.connect(g_szDeviceId);
+    if (fMqttConnected == false) {
+      Serial.print(" fail, rc=");
+      Serial.println(mqtt.state());
+      delay(1000);
+    }
+  }
+
+  if (fMqttConnected)
+  {
+    Serial.println(" success");
+    mqtt.subscribe(MQTT_TOPIC_SUBSCRIBE);
+    Serial.printf("Subcribe topic: %s\n", MQTT_TOPIC_SUBSCRIBE);
+    mqttPublish(MQTT_TOPIC_PUBLISH_DEVICE_STATUS, "1");
+  }
+  return mqtt.connected();
+}
 
 void setup() {
   Serial.begin(115200);
@@ -16,30 +101,48 @@ void setup() {
   pinMode(PUSH_BUTTON, INPUT_PULLUP);
   Wire.begin();
   lightMeter.begin(); 
-  Serial.println("System ready!");
+  WifiConnect();
+  mqttConnect();
+  Serial.println("System online");
 }
 
+unsigned long nTimerHelpButton = 0;
+unsigned long nTimerSendData = 0;
 void loop() {
-  Serial.println("Hello World!");
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(3000);
+  unsigned long nNow;
 
-  float fHumidity = dht.getHumidity();
-  float fTemperature = dht.getTemperature();
-  float lux = lightMeter.readLightLevel();
-  Serial.printf("Humidity: %.2f, Temperature: %.2f, Light: %.2f \n",
-      fHumidity, fTemperature, lux);
-
-  if (digitalRead(PUSH_BUTTON)==LOW)
+  mqtt.loop();
+  nNow = millis();
+  if ((nNow - nTimerHelpButton)>5000) // tiap 5 detik lakukan:
   {
-    Serial.println("Button pressed!");
-    digitalWrite(LED_RED, HIGH);
+    nTimerHelpButton = nNow;
+    if (digitalRead(PUSH_BUTTON)==LOW)
+    {
+      Serial.println("Button pressed!");
+      mqttPublish(MQTT_TOPIC_PUBLISH_HELP_BUTTON, "1");
+    }
+    else
+    {
+      mqttPublish(MQTT_TOPIC_PUBLISH_HELP_BUTTON, "0");
+    }
   }
-  else
+
+  nNow = millis();
+  if (nNow - nTimerSendData>10000) // tiap 10 detik lakukan:
   {
-    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_BUILTIN, HIGH);
+    nTimerSendData = nNow;
+    Serial.println("Send Data to MQTT Broker");
+
+    float fHumidity = dht.getHumidity();
+    float fTemperature = dht.getTemperature();
+    float lux = lightMeter.readLightLevel();
+    Serial.printf("Humidity: %.2f, Temperature: %.2f, Light: %.2f \n",
+        fHumidity, fTemperature, lux);
+    mqttPublish(MQTT_TOPIC_PUBLISH_TEMP, String(fTemperature).c_str());
+    mqttPublish(MQTT_TOPIC_PUBLISH_HUM, String(fHumidity).c_str());
+    mqttPublish(MQTT_TOPIC_PUBLISH_LIGHT, String(lux).c_str());        
+    digitalWrite(LED_BUILTIN, LOW);
   }
 }
 
